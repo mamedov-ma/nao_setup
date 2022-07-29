4da96c9b458187bf90e1a69178a8bd3281463e8d
Mamedov Mail <71010140+mamedov-ma@users.noreply.github.com>
2022-07-29 08:48:23 +0300

    Add files via upload

---
+++ py_pubsub/py_pubsub/walk_forward.py
@@ -0,0 +1,63 @@
+
+import rclpy
+from rclpy.logging import LoggingSeverity
+from rclpy.node import Node
+import time
+import numpy as np
+from std_msgs.msg import String
+from nao_sensor_msgs.msg import Touch
+from std_msgs.msg import ColorRGBA
+from nao_command_msgs.msg import ChestLed
+from nao_command_msgs.msg import JointStiffnesses
+from nao_command_msgs.msg import JointPositions
+
+
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__('walk_forward')
+        self.count = 0
+        self.subscription = self.create_subscription(Touch, 'sensors/touch', self.listener_callback, 10)
+        self.publisher_chest_led = self.create_publisher(ChestLed, 'effectors/chest_led', 10)
+        self.publisher_jointstiffnesses = self.create_publisher(JointStiffnesses, 'effectors/joint_stiffnesses', 10)
+        self.publisher_jointpositions = self.create_publisher(JointStiffnesses, 'effectors/joint_positions', 10)
+        self.poses = np.load(r'poses.npy')
+
+    def listener_callback(self, msg):
+        if (str(input()) = 'go'):
+            walk_act == 1
+        if (str(input()) = 'stop'):    
+            walk_act == 0 
+        if (walk_act == 1):  
+            chestled_color = ChestLed()
+            chestled_color.color.r = 1.0 if self.count % 3 == 0 else 0.0
+            chestled_color.color.g = 1.0 if self.count % 3 == 1 else 0.0
+            chestled_color.color.b = 1.0 if self.count % 3 == 2 else 0.0
+            self.publisher_chest_led.publish(chestled_color)       
+            send_pose(self)
+            time.sleep(0.012)
+            self.count = (self.count + 1) % self.poses.shape[1]
+            
+     def send_pose(self):
+        pose = pose_cicle(self.count)
+        positions_msg = JointPositions()
+        positions_msg.indexes = range(25)
+        positions_msg.positions = list(map(np.deg2rad, self.poses[self.count]))
+        print('send:', positions_msg)
+        self.publisher_jointpositions.publish(positions_msg)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
