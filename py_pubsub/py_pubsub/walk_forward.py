1f814f32bee03edf6cd83b3cd8227d8b3ff79d86
Mamedov Mail <71010140+mamedov-ma@users.noreply.github.com>
2022-08-02 01:49:06 +0300

    Update walk_forward.py

--- py_pubsub/py_pubsub/walk_forward.py
+++ py_pubsub/py_pubsub/walk_forward.py
@@ -1,4 +1,3 @@
-
 import rclpy
 from rclpy.logging import LoggingSeverity
 from rclpy.node import Node
@@ -20,9 +19,19 @@ class MyNode(Node):
         self.subscription = self.create_subscription(Touch, 'sensors/touch', self.listener_callback, 10)
         self.publisher_chest_led = self.create_publisher(ChestLed, 'effectors/chest_led', 10)
         self.publisher_jointstiffnesses = self.create_publisher(JointStiffnesses, 'effectors/joint_stiffnesses', 10)
-        self.publisher_jointpositions = self.create_publisher(JointStiffnesses, 'effectors/joint_positions', 10)
-        self.poses = np.load(r'poses.npy')
-
+        self.publisher_jointpositions = self.create_publisher(JointPositions, 'effectors/joint_positions', 10)
+        #self.poses = np.load(r'poses.npy')
+        self.poses = [[-9, 0, 8, 9, 0, -8], [-9, 0, -1, -40, 30, 8], [9, 0, -8, -9, 0, 8], [-40, 30, 8, -9, 0, -1]]
+    '''
+    def one_poses_cycle(self, msg):
+        for position in self.poses:
+            positions_msg = JointPositions()
+            positions_msg.indexes = [9, 10, 11, 14, 15, 16]
+            positions_msg.positions = list(map(np.deg2rad, position))
+            print('send:', positions_msg)
+            self.publisher_jointpositions.publish(positions_msg)
+            time.sleep(0.08)
+    '''
     def listener_callback(self, msg):
         '''
         if (str(input()) = 'go'):
@@ -31,23 +40,34 @@ class MyNode(Node):
             walk_act == 0 
         if (walk_act == 1):  
         '''
+       
         if msg.head_middle:
+            
+            if(self.count == 0):
+                joint_msg = JointStiffnesses()
+                joint_msg.indexes = range(25)
+                joint_msg.stiffnesses = [1.0] * 25
+                print(joint_msg)
+                self.publisher_jointstiffnesses.publish(joint_msg)
+            
             chestled_color = ChestLed()
             chestled_color.color.r = 1.0 if self.count % 3 == 0 else 0.0
             chestled_color.color.g = 1.0 if self.count % 3 == 1 else 0.0
             chestled_color.color.b = 1.0 if self.count % 3 == 2 else 0.0
             self.publisher_chest_led.publish(chestled_color)       
-            one_poses_cicle(self)
-            #self.count = (self.count + 1) % (self.poses.shape[0] - 1)
-            
-     def one_poses_cicle(self):
-        for position in self.poses:
-            positions_msg = JointPositions()
-            positions_msg.indexes = range(25)
-            positions_msg.positions = list(map(np.deg2rad, position)
-            print('send:', positions_msg)
-            self.publisher_jointpositions.publish(positions_msg)
-            time.sleep(0.02)
+            #one_poses_cycle(self, msg)
+            for position in self.poses:
+                positions_msg = JointPositions()
+                positions_msg.indexes = [9, 10, 11, 14, 15, 16]
+                positions_msg.positions = list(map(np.deg2rad, position))
+                print('send:', positions_msg)
+                self.publisher_jointpositions.publish(positions_msg)
+                time.sleep(0.5)
+            #time.sleep(0.08)
+            self.count += 1
+  
+     
+    
 
                                            
 def main(args=None):
@@ -62,4 +82,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
