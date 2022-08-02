930c9cec4b54b4d7bacb17128e4f5abd531ae886
Mamedov Mail <71010140+mamedov-ma@users.noreply.github.com>
2022-08-02 04:31:24 +0300

    Update walk_forward.py

--- py_pubsub/py_pubsub/walk_forward.py
+++ py_pubsub/py_pubsub/walk_forward.py
@@ -21,7 +21,14 @@ class MyNode(Node):
         self.publisher_jointstiffnesses = self.create_publisher(JointStiffnesses, 'effectors/joint_stiffnesses', 10)
         self.publisher_jointpositions = self.create_publisher(JointPositions, 'effectors/joint_positions', 10)
         #self.poses = np.load(r'poses.npy')
-        self.poses = [[-9, 0, 8, 9, 0, -8], [-9, 0, -1, -40, 30, 8], [9, 0, -8, -9, 0, 8], [-40, 30, 8, -9, 0, -1]]
+        self.poses = [[ -9 - 10,   0,    10,     9 - 10,    0,    -8,  80, 80], 
+                      [  0 - 10,   0,   4.5,   -13 - 10,   20,    -8,  80, 80], 
+                      [  9 - 10,   0,    -1,   -35 - 10,   40,    -8,  80, 80], 
+                      [  9 - 10,   0,  -4.5,   -22 - 10,   20,     1,  80, 80], 
+                      [  9 - 10,   0,    -8,    -9 - 10,    0,    10,  80, 80], 
+                      [-13 - 10,  20,    -8,     0 - 10,    0,   4.5,  80, 80], 
+                      [-35 - 10,  40,    -8,     9 - 10,    0,    -1,  80, 80], 
+                      [-22 - 10,  20,     1,     9 - 10,    0,  -4.5,  80, 80]]
     '''
     def one_poses_cycle(self, msg):
         for position in self.poses:
@@ -56,14 +63,15 @@ class MyNode(Node):
             chestled_color.color.b = 1.0 if self.count % 3 == 2 else 0.0
             self.publisher_chest_led.publish(chestled_color)       
             #one_poses_cycle(self, msg)
-            for position in self.poses:
-                positions_msg = JointPositions()
-                positions_msg.indexes = [9, 10, 11, 14, 15, 16]
-                positions_msg.positions = list(map(np.deg2rad, position))
-                print('send:', positions_msg)
-                self.publisher_jointpositions.publish(positions_msg)
-                time.sleep(0.5)
-            #time.sleep(0.08)
+            positions_msg = JointPositions()
+            positions_msg.indexes = [9, 10, 11, 14, 15, 16, 2, 18]
+            positions_msg.positions = list(map(np.deg2rad, self.poses[self.count % 8]))
+            print('send:', positions_msg)
+            self.publisher_jointpositions.publish(positions_msg)
+            if(self.count % 4 == 0):
+                time.sleep(0.1)
+            else:
+                time.sleep(0.1)
             self.count += 1
   
      
